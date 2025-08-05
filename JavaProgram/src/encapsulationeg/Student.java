593f4b4e13c2308d8b24db21160714a258728a7a
Ashwini.E <Ashwini.E@LAPTOP-00S06NK2>
2025-08-05 15:32:25 +0530

    my first day class

---
+++ JavaProgram/src/encapsulationeg/Student.java
@@ -0,0 +1,13 @@
+package encapsulationeg;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sid;
+		String sname;
+		float avg;
+		
+	}
+
+}
---
+++ JavaProgram/src/encapsulationeg/StudentDemo.java
@@ -0,0 +1,12 @@
+package encapsulationeg;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s=new Student();
+		System.out.println("");
+
+	}
+
+}
