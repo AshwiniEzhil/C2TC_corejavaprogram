8758ebd4ec0958fef233ad7226389c7547e87c55
Ashwini.E <Ashwini.E@LAPTOP-00S06NK2>
2025-08-19 14:09:18 +0530

    SingleInheritance is completed

---
+++ JavaProgram/src/singleinheritance_example/SingleInheritanceDemo.java
@@ -0,0 +1,14 @@
+package singleinheritance_example;
+
+public class SingleInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Trainer t=new Trainer(112,"Ashwini","puducherry",11,"B.Tech ICB","chennai");
+		System.out.println(t);
+		Person p=new Person(11,"ashi","puducherry");
+		System.out.println(p);
+
+	}
+
+}
